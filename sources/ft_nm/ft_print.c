0ac65ba59252afe6a0c5720cadb1e865ab861b3f
Jonathan Carra <jcarra@student.42.fr>
2018-03-09 16:26:05 +0100

    daw

--- sources/ft_nm/ft_print.c
+++ sources/ft_nm/ft_print.c
@@ -6,7 +6,7 @@
 /*   By: jcarra <jcarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/31 08:44:04 by jcarra            #+#    #+#             */
-/*   Updated: 2018/03/09 15:39:39 by jcarra           ###   ########.fr       */
+/*   Updated: 2018/03/09 16:07:27 by jcarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,34 @@ static void			ft_init(char (*tab)[17][31])
 	}
 }
 
+static void			ft_init_char(char (*tab)[17][31], uint32_t pos, char c,
+									char nb)
+{
+	if (nb == 1 || nb == 3 || nb == 5 || nb == 7)
+		(*tab)[pos][14] = c;
+	if (nb == 2 || nb == 3 || nb == 6 || nb == 7)
+		(*tab)[pos][15] = (char)(c - 'a' + 'A');
+	if (nb == 4 || nb == 5 || nb == 6 || nb == 7)
+		(*tab)[pos][30] = c;
+}
+
 extern char			ft_nsect_decription(uint32_t n_sect, uint32_t n_type)
 {
 	char		tab[17][31];
 
 	ft_init(&tab);
+	ft_init_char(&tab, 1, 't', 7);
+	ft_init_char(&tab, 4, 's', 1);
+	ft_init_char(&tab, 5, 's', 1);
+	ft_init_char(&tab, 6, 's', 2);
+	ft_init_char(&tab, 8, 'd', 2);
+	ft_init_char(&tab, 9, 'b', 3);
+	ft_init_char(&tab, 10, 's', 2);
+	ft_init_char(&tab, 11, 'd', 3);
+	ft_init_char(&tab, 12, 'b', 1);
+	ft_init_char(&tab, 12, 's', 2);
+
+	/*
 	tab[1][14] = 't';
 	tab[1][15] = 'T';
 	tab[1][30] = 't';
@@ -49,11 +72,20 @@ extern char			ft_nsect_decription(uint32_t n_sect, uint32_t n_type)
 	tab[11][15] = 'D';
 	tab[12][14] = 'b';
 	tab[12][15] = 'S';
+	 */
+
+	ft_init_char(&tab, 13, 'd', 7);
+	ft_init_char(&tab, 14, 's', 4);
+	ft_init_char(&tab, 15, 'b', 1);
+	ft_init_char(&tab, 16, 's', 2);
+
+	/*
 	tab[13][14] = 'd';
 	tab[13][15] = 'D';
 	tab[13][30] = 'd';
 	tab[14][30] = 's';
 	tab[15][14] = 'b';
 	tab[16][15] = 'S';
+	 */
 	return ((n_sect > 16 || n_type > 30) ? (char)' ' : tab[n_sect][n_type]);
 }
